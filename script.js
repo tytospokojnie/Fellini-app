3868cc15672da22eaa5de1c6d9e6ee32fbe56a7c
tytospokojnie <tymoschodo@gmail.com>
2025-02-11 12:17:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -128,9 +128,8 @@ document.addEventListener("DOMContentLoaded", function () {
         popupContent.innerHTML = contentMap[id] || "No content available";
         popup.style.display = "block";
         popup.style.position = "absolute";
-        popup.style.left = "200px";
         popup.style.top = "100px";
-        popup.style.width = "500px";
+        popup.style.width = "400px";
     }
 });
 
